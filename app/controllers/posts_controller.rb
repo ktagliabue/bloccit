4fa542d8db7ded5eb80651811406a92ce7698498
Kandis <ktagliabue@hotmail.com>
2014-10-16 09:57:17 -0700

    posts mods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
  	def index
   	@posts = Post.all
   end
 
   def show
-  	@post = Post.find(params[:id])
   end
 
   def new
@@ -12,7 +13,7 @@ class PostsController < ApplicationController
   end
 
   def create
-     @post = Post.new(params.require(:post).permit(:title, :body))
+     @post = current_user.posts.build(post_params)
      if @post.save
        flash[:notice] = "Post was saved."
        redirect_to @post
@@ -23,18 +24,26 @@ class PostsController < ApplicationController
    end
 
   def edit
-  	@post = Post.find(params[:id])
   end
 
   def update #could I inherit from another method here? @adam
-     @post = Post.find(params[:id])
-     if @post.update_attributes(params.require(:post).permit(:title, :body))
-       flash[:notice] = "Post was updated."
-       redirect_to @post
-     else
-       flash[:error] = "There was an error saving the post. Please try again."
-       render :edit
-     end
-   end
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
+
+
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 
 end
